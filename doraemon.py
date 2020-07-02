f20e1f5a518bf3669d6cdd9a04aec0f16d1e8b18
Pratish <pratishrai724@gmail.com>
2020-07-03 00:31:27 +0530

    logs

--- doraemon.py
+++ doraemon.py
@@ -8,14 +8,7 @@ import fun
 from discord.utils import find
 
 
-def get_prefix(client, message):
-    with open('prefixes.json', 'r') as file:
-        prefixes = json.load(file)
-
-    return prefixes[str(message.guild.id)]
-
-
-client = commands.Bot(command_prefix=get_prefix)
+client = commands.Bot(command_prefix="-")
 analytics = prismapy.Prismalytics("Key", client, save_server=True)
 client.remove_command('help')
 
@@ -37,59 +30,22 @@ async def on_ready():
 async def on_message(message):
     ctx = await client.get_context(message)
     if ctx.valid:
-        print(
-            f'{message.guild} : {message.author} : {message.content} : {message.created_at.strftime("%a, %#d %B %Y, %I:%M %p UTC")}')
+        f = open("logs.txt", "a", newline="")
+        f.write(f'{message.guild} : {message.author} : {message.content} : {message.created_at.strftime("%a, %#d %B %Y, %I:%M %p UTC")}\n')
+        f.close()
     await client.process_commands(message)
 
 
 @client.event
 async def on_guild_join(guild):
-    with open('prefixes.json', 'r') as file:
-        prefixes = json.load(file)
-
-    prefixes[str(guild.id)] = '-'
-
-    with open('prefixes.json', 'w') as file:
-        json.dump(prefixes, file, indent=4)
-
     general = find(lambda x: x.name == 'general', guild.text_channels)
     if general and general.permissions_for(guild.me).send_messages:
         embed = discord.Embed(
             title="Hello!",
             colour=0x2859b8,
-            description="""Hello, I'm **Doraemon**, I am a multi functional bot. I can be used for Fun, Moderation and much more. My default command prefix is `-`.
-You can, however, change it. You can find all my command by typing `-help` and know more about me by typing `-about`. 
-    
-    """)
+            description="""Hello, I'm **Doraemon**. I am a bot made by **Pratish**. I can be used for Fun, Moderation and much more. My command prefix is `-`.
+You can find all my command by typing `-help` and know more about me by typing `-about`. """)
         await general.send(embed=embed)
-
-
-@client.event
-async def on_guild_remove(guild):
-    with open('prefixes.json', 'r') as file:
-        prefixes = json.load(file)
-
-    prefixes.pop(str(guild.id))
-
-    with open('prefixes.json', 'w') as file:
-        json.dump(prefixes, file, indent=4)
-
-
-@client.command(aliases=['prefix'])
-@commands.has_permissions(manage_guild=True)
-async def change_prefix(ctx, prefix):
-    with open('prefixes.json', 'r') as file:
-        prefixes = json.load(file)
-
-    prefixes[str(ctx.guild.id)] = prefix
-
-    with open('prefixes.json', 'w') as file:
-        json.dump(prefixes, file, indent=4)
-    embed = discord.Embed(
-        title="Prefix",
-        colour=0x2859b8,
-        description=f'Prefix changed to: `{prefix}` on {ctx.guild}')
-    await ctx.send(embed=embed)
 
 
 @client.event
@@ -108,13 +64,13 @@ async def on_command_error(ctx, error):
         embed = discord.Embed(
             title="Error",
             colour=0x2859b8,
-            description="This command doesn't exist.")
+            description="Sorry, This command doesn't exist.")
         await ctx.send(embed=embed)
     elif isinstance(error, commands.MissingPermissions):
         embed = discord.Embed(
             title="Error",
             colour=0x2859b8,
-            description="You don't have the permissions to use this command.")
+            description="Sorry, You don't have the permissions to use this command.")
         await ctx.send(embed=embed)
 
 
@@ -126,8 +82,7 @@ async def about(ctx):
         description="""Hello, I'm **Doraemon**,
 I am not a 22nd century bot, I have been built in 21st century by [**Pratish**](http://programmingwizard.tech/).
 I am a multi functional bot. I can be used for Fun, Moderation and much more.
-Use the `help` command to know my commands and their functions.
-Please [`invite`](https://discord.com/api/oauth2/authorize?client_id=709321027775365150&permissions=8&scope=bot) me to your server:
+Use the `-help` command to know my commands and their functions.
 """)
     await ctx.send(embed=embed)
 
@@ -138,29 +93,29 @@ async def help(ctx):
         title="Doraemon's commands:",
         colour=0x2859b8,
         description="""
-> To use a  command type `<prefix><command>`. The default prefix is `-`.
 
 **__General__**
 
-`about` - To know about the bot.
-`ping` - Check the bot's latency.
-`github` - Github Repo.
-`stats` - Check the bot's stats.
-`count` - Count of messages in a channel.
+`-about` - To know about the bot.
+`-ping` - Check the bot's latency.
+`-github` - Github Repo.
+`-stats` - Check the bot's stats.
+`-invite` - Get the invite link for the bot.
 
 **__Fun__**
 
-`udict <term>` - Get the definition ot the terms from Urban Dictionary.
-`8ball <your question>` - Play magic 8 Ball and get the answers to all your questions.
+`-udict <term>` - Get the definition ot the terms from Urban Dictionary.
+`-8ball <your question>` - Play magic 8 Ball and get the answers to all your questions.
+`-lmgtfy <question>` -  Returns a [lmgtfy.com](https://lmgtfy.com/) link.
 
 **__Moderation__**
 
-`clear <amount of messages>` - Clears the specified no. of messages.(default=5)
-`kick <member> <reason>` - Kicks a member out of the server.
-`ban <member> <reason>` - Bans a member in the server.
-`unban <member>` - Unbans the member in the server.
-`prefix <new prefix>` - Changes the Prefix for a specific server.
-`info` - General Info of a member.
+`-clear <amount of messages>` - Clears the specified no. of messages.(default=5)
+`-kick <member> <reason>` - Kicks a member out of the server.
+`-ban <member> <reason>` - Bans a member in the server.
+`-unban <member>` - Unbans the member in the server.
+`-count` - Count of messages in a channel.
+`-info` - General Info of a member.
 """)
     await ctx.send(embed=embed)
 
@@ -180,6 +135,14 @@ async def github(ctx):
         title="GitHub Repo",
         colour=0x2859b8,
         description="https://github.com/programming-wizard/doraemon")
+    await ctx.send(embed=embed)
+
+
+@client.command()
+async def invite(ctx):
+    embed = discord.Embed(
+        colour=0x2859b8,
+        description="https://discord.com/api/oauth2/authorize?client_id=709321027775365150&permissions=8&scope=bot) me to your server")
     await ctx.send(embed=embed)
 
 
@@ -228,8 +191,8 @@ async def udict(ctx, term):
                     value=f"{query['author']}")
     embed.add_field(name="**__Votes__**", value=f"{query['thumbs_up']} :thumbsup: {query['thumbs_down']} :thumbsdown:")
     await ctx.send(embed=embed)
-
-
+    
+    
 @client.command()
 async def lmgtfy(ctx, *, question):
 	ques = question.replace(" ", "+")
@@ -335,3 +298,4 @@ for filename in os.listdir('./cogs'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
 client.run('Bot Token')
+
